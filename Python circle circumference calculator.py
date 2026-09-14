abe3f9b2182a49f900224f0f53d34c15da296109
pg929348-hash <pg929348@gmail.com>
2026-09-14 17:13:39 +0530

    Change π to 22/7 for circumference calculation

--- Python circle circumference calculator.py
+++ Python circle circumference calculator.py
@@ -2,7 +2,7 @@ print(" ===CIRCLE CIRCUMFERENCE CALCULATER===")
 while True:
       try:
           r=int(input("enter the radius of a circle:-"))
-          b=2*π*r
+          b=2*22/7*r
           print(" the circumference of the circle is :-",round(b,2))
       except:
            print(" enter number only not units okay")
